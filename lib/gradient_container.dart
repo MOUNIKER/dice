2343d590a206f3efe6a08bced3f7454a68822bef
VANUKURI SIVA MOUNIKER <105932128+MOUNIKER@users.noreply.github.com>
2023-08-04 09:57:18 +0530

    removed errors

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,31 +25,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
+      child: const Center(
         child: DiceRoller(),
       ),
     );
   }
 }
-
-// class GradientContainer extends StatelessWidget {
-//   const GradientContainer({super.key, required this.colors});
-
-//   final List<Color> colors;
-
-//   @override
-//   Widget build(context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         gradient: LinearGradient(
-//           colors: colors,
-//           begin: startAlignment,
-//           end: endAlignment,
-//         ),
-//       ),
-//       child: const Center(
-//         child: StyledText('Hello World!'),
-//       ),
-//     );
-//   }
-// }
